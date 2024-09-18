f4cd22c214be6e59076bd72c176b4608f528155e
Franck Lamy <francklamymagie@gmail.com>
2024-09-18 16:34:19 +0200

    install homePage

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import './Styles/Home.scss';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <header className="sectionIntro">
+        <p>Le petit éditeur lillois qui a de la bouteille</p>
+      </header>
+
+    </div>
+  );
+};
+
+export default Home;
