b9e32a0cab38e15e7c48d853c576b51b0a42530d
Franck Lamy <francklamymagie@gmail.com>
2024-09-20 11:02:02 +0200

    first commit form

---
+++ src/Components/Formulaire.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../Styles/Formulaire.scss";
+
+const Formulaire =()=>{
+
+    return(
+        <div>
+
+            <h1>Formulaire de contact</h1>
+            
+        
+        </div>
+
+    )
+};
+
+export default Formulaire;
--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,12 +1,26 @@
 import React from "react";
+import Formulaire from "../Components/Formulaire";
+import "../Styles/Contact.scss";
 
 const Contact=()=>{
     return(
         <div class="content">
+        <h1 className="titrePage">Contactez-nous</h1>
+        <div className="textContact">
+        <h2 className="sousTitre">Nous sommes à votre écoute !</h2>
+            <p className="paragraphe">
+            Pour toute question, suggestion ou demande, n’hésitez pas à nous contacter. Nous serons ravis de vous répondre.</p>
 
-
-        <h1>Contact</h1>
         </div>
+
+        <Formulaire/>
+
+        <p>Coordonnées :
+Adresse :Les Lumières de Lille3 avenue Poincaré, 59700 Marcq-en-Barœul, France
+Email : contact@lumieresdelille.fr
+Téléphone : +33 (0)3 XX XX XX XX</p>
+
+    </div>
     )
 
 };
