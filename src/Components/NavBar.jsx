43cc8dc09650b5ade4f6dac4911401253c8445e1
Franck Lamy <francklamymagie@gmail.com>
2024-09-23 16:05:33 +0200

    responsiv Header

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,21 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "../Styles/NavBar.scss";
 
 const Navbar = () => {
+    // État pour gérer l'ouverture/fermeture du menu sur les petits écrans
+    const [isOpen, setIsOpen] = useState(false);
+
+    // Fonction pour basculer l'état du menu
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <nav className="navBar">
-            <Link to="/nos_autrices_auteurs" className="buttonNavBar">
-            <button className="buttonNavBar">Nos Autrices/Auteurs</button>
-            </Link>
+            {/* Logo ou autre contenu en haut si besoin */}
+            
+            {/* Bouton burger visible sur petits écrans */}
+            <div className={`burger ${isOpen ? "open" : ""}`} onClick={toggleMenu}>
+                <span className="bar"></span>
+                <span className="bar"></span>
+                <span className="bar"></span>
+            </div>
 
-            <Link to="/vos_avis"className="buttonNavBar">
-            <button className="buttonNavBar">Vos Avis</button>
-            </Link>
+            {/* Menu de navigation, affiché ou caché en fonction de l'état */}
+            <div className={`navLinks ${isOpen ? "open" : ""}`}>
+                <Link to="/nos_autrices_auteurs" className="buttonNavBar">
+                    <button className="buttonNavBar">Nos Autrices/Auteurs</button>
+                </Link>
 
-            <Link to="/contact" className="buttonNavBar">
-            <button className="buttonNavBar">Contact</button>
-            </Link>
+                <Link to="/vos_avis" className="buttonNavBar">
+                    <button className="buttonNavBar">Vos Avis</button>
+                </Link>
+
+                <Link to="/contact" className="buttonNavBar">
+                    <button className="buttonNavBar">Contact</button>
+                </Link>
+            </div>
         </nav>
     );
 };
