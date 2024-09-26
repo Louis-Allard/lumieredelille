0931b82059cb7b0f8b58863ed7bdf5c4ebc941e9
Franck Lamy <francklamymagie@gmail.com>
2024-09-26 14:51:58 +0200

    fix: conditionally display singular or plural for 'ARTICLE(S)' based on item count

--- src/Components/Panier.jsx
+++ src/Components/Panier.jsx
@@ -3,12 +3,15 @@ import LogoPanier from "../Assets/paniers.png";
 import "../Styles/Panier.scss";
 
 const Panier = () => {
+
+    let nbArticles = 0;
+
     return (
         <div className="panier">
             <img src={LogoPanier} alt="Panier" className="logoPanier" />
             <div className="textPanier">
                 <p className="titrePanier">Mon panier</p>
-                <p className="articlePanier">0 ARTICLE</p>
+                <p className="articlePanier">{nbArticles} {nbArticles<=1? "ARTICLE": "ARTICLES"} </p>
             </div>
         </div>
     );
